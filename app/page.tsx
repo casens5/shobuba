a68eae11892b18ad36f9ca13b67efcd27ea07da7
casens5 <rudolfordoyne@protonmail.com>
2024-12-22 10:50:30 -0600

    fix background/foreground for light mode users

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Game from "./game";
 
 export default function Home() {
   return (
-    <div className="flex h-full min-h-screen flex-col items-center justify-center sm:p-8">
+    <div className="flex h-full min-h-screen flex-col items-center justify-center sm:p-8 bg-zinc-800 text-zinc-50">
       <main className="flex h-full w-full flex-1 flex-col items-center text-xl">
         <Game />
       </main>
